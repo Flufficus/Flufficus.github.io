bd44317c15996edeeca2001efe3c80938dcf5c68
Flufficus <35151910+Flufficus@users.noreply.github.com>
2019-08-22 14:20:40 +0100

    fixed code

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 
 <?php
- 
+
 $ip = $_SERVER['REMOTE_ADDR'];
-    
+
 $file = "ips.txt";
 $text = file_get_contents($file);
-$text .=$ip . "\n";
+$text .= $ip . "\n";
 file_put_contents($file, $text);
 
 ?>
