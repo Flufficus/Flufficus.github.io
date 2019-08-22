eab150c91d9fd217323487ff5338fdf24303a293
Flufficus <35151910+Flufficus@users.noreply.github.com>
2019-08-22 14:15:39 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
 
 <?php
  
-//IP Grabber
- 
-//Variables
- 
-$protocol = $_SERVER['SERVER_PROTOCOL'];
 $ip = $_SERVER['REMOTE_ADDR'];
-$port = $_SERVER['REMOTE_PORT'];
-$agent = $_SERVER['HTTP_USER_AGENT'];
-$ref = $_SERVER['HTTP_REFERER'];
-$hostname = gethostbyaddr($_SERVER['REMOTE_ADDR']);
- 
-//Print IP, Hostname, Port Number, User Agent and Referer To Log.TXT
- 
-$fh = fopen('log.txt', 'a');
-fwrite($fh, 'IP Address: '."".$ip ."\n");
-fwrite($fh, 'Hostname: '."".$hostname ."\n");
-fwrite($fh, 'Port Number: '."".$port ."\n");
-fwrite($fh, 'User Agent: '."".$agent ."\n");
-fwrite($fh, 'HTTP Referer: '."".$ref ."\n\n");
-fclose($fh);
+    
+$file = "ips.txt";
+$text = file_get_contents($file);
+$text .=$ip . "\n";
+file_put_contents($file, $text);
+
 ?>
